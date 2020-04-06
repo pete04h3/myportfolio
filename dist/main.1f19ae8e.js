6b12bfd89ecd8ebe647544ce1f6aac99f7e5daf6
peterpaN1k <peetmj@gmail.com>
2020-04-06 16:46:05 +0200

    Added animations and welcome screen

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -144,7 +144,17 @@ window.addEventListener("DOMContentLoaded", init);
 function init() {
   console.log(init);
   document.querySelector("body > main > section.section-3 > div").style.margin = "0px";
-  /* document.querySelector("body > main > section.section-1 > img").classList.add("zoomIneffect"); */
+  document.querySelector("#welcomescreen > div > p").addEventListener("click", removeWelcome);
+}
+
+function removeWelcome() {
+  document.querySelector(".hero").classList.add("fade-out");
+  setTimeout(removeWelcomeScreen, 2000);
+}
+
+function removeWelcomeScreen() {
+  document.querySelector("#welcomescreen").classList.add("hide");
+  document.querySelector(".hero").style.zIndex = "-1";
 }
 },{}],"main.js":[function(require,module,exports) {
 "use strict";
@@ -215,7 +225,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50668" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54188" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- modules/portfolio.js
+++ modules/portfolio.js
@@ -5,5 +5,16 @@ window.addEventListener("DOMContentLoaded", init);
 function init() {
   console.log(init);
   document.querySelector("body > main > section.section-3 > div").style.margin = "0px";
-  /* document.querySelector("body > main > section.section-1 > img").classList.add("zoomIneffect"); */
+
+  document.querySelector("#welcomescreen > div > p").addEventListener("click", removeWelcome);
+}
+
+function removeWelcome() {
+  document.querySelector(".hero").classList.add("fade-out");
+  setTimeout(removeWelcomeScreen, 2000);
+}
+
+function removeWelcomeScreen() {
+  document.querySelector("#welcomescreen").classList.add("hide");
+  document.querySelector(".hero").style.zIndex = "-1";
 }
