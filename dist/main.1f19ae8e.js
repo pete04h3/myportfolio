609b4404af52c7e8019104227975681e47118c9a
peterpaN1k <peetmj@gmail.com>
2020-04-06 12:08:59 +0200

    added error in gif section1

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -144,9 +144,7 @@ window.addEventListener("DOMContentLoaded", init);
 function init() {
   console.log(init);
   document.querySelector("body > main > section.section-3 > div").style.margin = "0px";
-  $(".menu-activator").on("click", function () {
-    $("body").toggleClass("menu-active");
-  });
+  /* document.querySelector("body > main > section.section-1 > img").classList.add("zoomIneffect"); */
 }
 },{}],"main.js":[function(require,module,exports) {
 "use strict";
@@ -217,7 +215,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54006" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63400" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- modules/portfolio.js
+++ modules/portfolio.js
@@ -5,8 +5,5 @@ window.addEventListener("DOMContentLoaded", init);
 function init() {
   console.log(init);
   document.querySelector("body > main > section.section-3 > div").style.margin = "0px";
-
-  $(".menu-activator").on("click", function() {
-    $("body").toggleClass("menu-active");
-  });
+  /* document.querySelector("body > main > section.section-1 > img").classList.add("zoomIneffect"); */
 }
