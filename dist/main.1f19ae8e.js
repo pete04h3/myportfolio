f0eaaa704e48502ee98260316e7dc85101c6a356
peterpaN1k <peetmj@gmail.com>
2020-04-09 12:55:29 +0200

    fixed z-index on hero

--- dist/main.1f19ae8e.js
+++ dist/main.1f19ae8e.js
@@ -333,7 +333,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61589" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63088" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
