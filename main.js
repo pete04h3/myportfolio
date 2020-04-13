e5e603f02b0831536ca3662affc2bbbd64aa5ae0
peterpaN1k <peetmj@gmail.com>
2020-04-13 19:21:46 +0200

    initial commit

--- main.js
+++ main.js
@@ -4,6 +4,19 @@ import { gallery as myImgs } from "./modules/gallery";
 
 const data = [
   {
-    i: ""
-  }
+    i: "",
+  },
 ];
+
+let nav = document.querySelector(".nav-list");
+let bar = document.querySelector(".nav-bar");
+let clicked = false;
+bar.addEventListener("click", function () {
+  if (!clicked) {
+    clicked = true;
+    nav.style.left = "0%";
+  } else {
+    clicked = false;
+    nav.style.left = "-100%";
+  }
+});
